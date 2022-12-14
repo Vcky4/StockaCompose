fd06d2dbb2005448f2b2836d262c1457e50ab559
Vcky4 <okon.victor.u@gmail.com>
2022-12-14 22:24:03 +0100

    add logo to intro screen

--- app/src/main/java/com/vicksoson/stockacompose/MainActivity.kt
+++ app/src/main/java/com/vicksoson/stockacompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.vicksoson.stockacompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vicksoson.stockacompose.ui.theme.StockaComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,12 +29,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Intro()
                 }
             }
         }
     }
 }
+
+
+@Composable
+fun Intro() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 38.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.stocka_logo),
+            contentDescription = "Stocka Logo",
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+    }
+}
+
 
 @Composable
 fun Greeting(name: String) {
