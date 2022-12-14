32857252fa0f186cda6ec2801d502d6e0f8ba365
Vcky4 <okon.victor.u@gmail.com>
2022-12-14 22:01:07 +0100

    splash done

--- app/src/main/java/com/vicksoson/stockacompose/MainActivity.kt
+++ app/src/main/java/com/vicksoson/stockacompose/MainActivity.kt
@@ -18,12 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             StockaComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Greeting("Android")
             }
         }
     }
