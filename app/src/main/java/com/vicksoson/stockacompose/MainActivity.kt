5804cd7b1e2374e405e65c8f6eb191e352add96a
Vcky4 <okon.victor.u@gmail.com>
2022-12-15 22:01:35 +0100

    almost done

--- app/src/main/java/com/vicksoson/stockacompose/MainActivity.kt
+++ app/src/main/java/com/vicksoson/stockacompose/MainActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -20,6 +18,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.vicksoson.stockacompose.ui.theme.Black
 import com.vicksoson.stockacompose.ui.theme.StockaComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,6 +61,43 @@ fun Intro() {
                 fontWeight = FontWeight.Bold,
             )
         )
+
+        Column(Modifier.padding(top = 50.dp)) {
+            (0..3).map { each ->
+                Row(
+                    modifier = Modifier.padding(vertical = 12.dp),
+                    verticalAlignment = Alignment.Top
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.tick_box),
+                        contentDescription = "Tick Box"
+                    )
+                    Text(
+                        text = when (each) {
+                            0 -> "Forecast Profit"
+                            1 -> "Scan goods at the point of purchase"
+                            2 -> "Stay up to date with daily charts"
+                            else -> "Connect with other business owners"
+                        },
+                        modifier = Modifier.padding(start = 12.dp, end = 30.dp),
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Medium,
+                            color = Black
+                        )
+                    )
+                }
+            }
+        }
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 80.dp),
+        ) {
+            Text(text = "Continue", style = TextStyle(color = Black))
+        }
 
 
     }
