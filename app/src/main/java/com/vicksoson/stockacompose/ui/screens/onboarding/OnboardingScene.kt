bbe8daf3b42e810098c318c201415e7b541506cc
Vcky4 <okon.victor.u@gmail.com>
2022-12-26 22:53:02 +0100

    we have done alot

--- app/src/main/java/com/vicksoson/stockacompose/ui/screens/onboarding/OnboardingScene.kt
+++ app/src/main/java/com/vicksoson/stockacompose/ui/screens/onboarding/OnboardingScene.kt
@@ -1,10 +1,7 @@
 package com.vicksoson.stockacompose.ui.screens.onboarding
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
@@ -12,34 +9,142 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.*
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
+import com.vicksoson.stockacompose.R
 import com.vicksoson.stockacompose.ui.theme.Black
+import com.vicksoson.stockacompose.ui.theme.Gray
 import com.vicksoson.stockacompose.ui.theme.Primary
+import com.vicksoson.stockacompose.ui.theme.Teal200
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun OnboardingScene() {
-    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+    Column(Modifier.fillMaxSize()) {
         val pagerState = rememberPagerState()
-        HorizontalPager(count = 3, state = pagerState) {
-            Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-                Image(painter = painterResource(id = ), contentDescription = )
+        HorizontalPager(
+            count = 3, state = pagerState,
+            modifier = Modifier.weight(1f)
+        ) { page ->
+            Column(
+                Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(
+                        id = when (page) {
+                            0 -> R.drawable.bro
+                            1 -> R.drawable.amico
+                            else -> R.drawable.onbaording3
+                        }
+                    ), contentDescription = "onboarding image", modifier = Modifier.size(223.dp)
+                )
+                CustomText(
+                    text = when (page) {
+                        0 -> "Manage your stock on a daily, weekly and monthly basis"
+                        1 -> "Monitor your business growth, in real time."
+                        else -> "Be a step ahead in your business with stocka"
+                    },
+                    modifier = Modifier
+                        .widthIn(max = 301.dp)
+                        .padding(top = 67.dp),
+                )
+
             }
         }
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = 80.dp),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = Primary
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            HorizontalPagerIndicator(
+                pagerState = pagerState,
+                activeColor = Primary,
+                indicatorWidth = 12.7.dp,
+                indicatorHeight = 12.7.dp,
+                inactiveColor = Gray
             )
-        ) {
-            Text(text = "Continue", style = TextStyle(color = Black))
+            Button(
+                onClick = {  },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 80.dp, top = 56.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Primary
+                )
+            ) {
+                Text(
+                    text = when (pagerState.currentPage) {
+                        2 -> "Get Started"
+                        else -> "Next"
+                    }, style = TextStyle(color = Black)
+                )
+            }
         }
     }
+}
+
+@Composable
+fun CustomText(modifier: Modifier, text: String) {
+    val annotedText = buildAnnotatedString {
+        text.split(" ").forEach {
+            if (it == "stocka") {
+                "stocka".forEach { char ->
+                    if (char == 'a') {
+                        pushStringAnnotation(
+                            tag = "stocka",
+                            annotation = char.toString()
+                        )
+                        withStyle(
+                            style = SpanStyle(
+                                color = Primary,
+                                fontSize = 22.sp,
+                                fontWeight = FontWeight.SemiBold,
+
+                                )
+                        ) {
+                            append("$char")
+                        }
+                        pop()
+                    } else {
+                        pushStringAnnotation(
+                            tag = "stocka",
+                            annotation = char.toString()
+                        )
+                        withStyle(
+                            style = SpanStyle(
+                                color = Teal200,
+                                fontSize = 22.sp,
+                                fontWeight = FontWeight.SemiBold,
+
+                                )
+                        ) {
+                            append("$char")
+                        }
+                        pop()
+                    }
+                }
+            } else {
+                pushStringAnnotation(
+                    tag = "URL",
+                    annotation = it
+                )
+                withStyle(
+                    style = SpanStyle(
+                        color = Black,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                ) {
+                    append("$it ")
+                }
+                pop()
+            }
+        }
+
+    }
+    Text(text = annotedText, modifier = modifier, textAlign = TextAlign.Center)
 }
