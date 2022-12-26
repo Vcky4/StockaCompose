cd7f07c06bbacb093d2c61ffb33937ac17f17a18
Vcky4 <okon.victor.u@gmail.com>
2022-12-26 21:51:22 +0100

    add images for OnboardingScene

--- app/src/main/java/com/vicksoson/stockacompose/ui/screens/onboarding/OnboardingScene.kt
+++ app/src/main/java/com/vicksoson/stockacompose/ui/screens/onboarding/OnboardingScene.kt
@@ -1,5 +1,6 @@
 package com.vicksoson.stockacompose.ui.screens.onboarding
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,15 +11,25 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import com.vicksoson.stockacompose.ui.theme.Black
 import com.vicksoson.stockacompose.ui.theme.Primary
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun OnboardingScene() {
     Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
-
+        val pagerState = rememberPagerState()
+        HorizontalPager(count = 3, state = pagerState) {
+            Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+                Image(painter = painterResource(id = ), contentDescription = )
+            }
+        }
         Button(
             onClick = { /*TODO*/ },
             modifier = Modifier
