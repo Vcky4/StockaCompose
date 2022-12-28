b72413626fd5082e40d47c952a38335bd472ad76
Vcky4 <okon.victor.u@gmail.com>
2022-12-28 22:36:06 +0100

    create nav

---
+++ app/src/main/java/com/vicksoson/stockacompose/ui/screens/navigation/MainNav.kt
@@ -0,0 +1,26 @@
+package com.vicksoson.stockacompose.ui.screens.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.vicksoson.stockacompose.ui.screens.authentication.Login
+import com.vicksoson.stockacompose.ui.screens.onboarding.Intro
+import com.vicksoson.stockacompose.ui.screens.onboarding.OnboardingScene
+
+@Composable
+fun MainNav() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "intro") {
+        composable("intro") {
+            Intro()
+        }
+        composable("onboarding") {
+            OnboardingScene()
+        }
+        composable("login") {
+            Login()
+        }
+    }
+}
