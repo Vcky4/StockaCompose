0f732fb7a2e23328a06e4e1c1ce3c3a44c02d3d5
Vcky4 <okon.victor.u@gmail.com>
2022-12-28 22:37:24 +0100

    add nav to main activity

--- app/src/main/java/com/vicksoson/stockacompose/MainActivity.kt
+++ app/src/main/java/com/vicksoson/stockacompose/MainActivity.kt
@@ -6,13 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.vicksoson.stockacompose.ui.screens.authentication.Login
+import com.vicksoson.stockacompose.ui.screens.navigation.MainNav
 import com.vicksoson.stockacompose.ui.screens.onboarding.Intro
-import com.vicksoson.stockacompose.ui.screens.onboarding.OnboardingScene
 import com.vicksoson.stockacompose.ui.theme.StockaComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Login()
+                    MainNav()
                 }
             }
         }
