76d98fd6b337aed62ae835cdbff1e9d8a22c48d5
Vcky4 <okon.victor.u@gmail.com>
2022-12-28 21:30:07 +0100

    stocka logo done

--- app/src/main/java/com/vicksoson/stockacompose/ui/screens/authentication/Login.kt
+++ app/src/main/java/com/vicksoson/stockacompose/ui/screens/authentication/Login.kt
@@ -1,11 +1,10 @@
 package com.vicksoson.stockacompose.ui.screens.authentication
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -13,11 +12,25 @@ import com.vicksoson.stockacompose.R
 
 @Composable
 fun Login() {
-    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween) {
-        Image(
-            painter = painterResource(id = R.drawable.stocka_logo),
-            contentDescription = "stocka logo",
-            modifier = Modifier.size(48.dp)
-        )
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(horizontal = 23.dp, vertical = 44.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Card(
+            Modifier
+                .padding(top = 10.dp, end = 18.dp)
+                .align(Alignment.End)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.stocka_logo),
+                contentDescription = "stocka logo",
+                modifier = Modifier
+                    .size(48.dp)
+
+            )
+        }
+
     }
 }
