2a938e45cec2dd69ddf7a85e703c6148785fc91d
Vcky4 <okon.victor.u@gmail.com>
2022-12-28 21:56:28 +0100

    fix

--- app/src/main/java/com/vicksoson/stockacompose/ui/screens/authentication/Login.kt
+++ app/src/main/java/com/vicksoson/stockacompose/ui/screens/authentication/Login.kt
@@ -3,11 +3,18 @@ package com.vicksoson.stockacompose.ui.screens.authentication
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.vicksoson.stockacompose.R
 
 @Composable
@@ -18,6 +25,10 @@ fun Login() {
             .padding(horizontal = 23.dp, vertical = 44.dp),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
+        var userName by remember {
+            mutableStateOf(TextFieldValue(""))
+        }
+        // Logo
         Card(
             Modifier
                 .padding(top = 10.dp, end = 18.dp)
@@ -31,6 +42,23 @@ fun Login() {
 
             )
         }
+
+        // Login Form
+        Column(Modifier.fillMaxWidth()) {
+            //greeting
+            Text(text = "Welcome back!", fontSize = 24.sp, fontWeight = FontWeight.Medium)
+
+            //username
+            TextField(
+                value = userName,
+                onValueChange = {
+                    userName = it },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                ),
+            )
+        }
+
 
     }
 }
