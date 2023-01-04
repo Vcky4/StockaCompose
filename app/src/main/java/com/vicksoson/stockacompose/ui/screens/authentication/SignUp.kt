28179e136c00b26ee8ae502c7294f40d2b13e208
Vcky4 <okon.victor.u@gmail.com>
2023-01-04 22:53:07 +0100

    sign up done

--- app/src/main/java/com/vicksoson/stockacompose/ui/screens/authentication/SignUp.kt
+++ app/src/main/java/com/vicksoson/stockacompose/ui/screens/authentication/SignUp.kt
@@ -1,7 +1,208 @@
 package com.vicksoson.stockacompose.ui.screens.authentication
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.vicksoson.stockacompose.R
+import com.vicksoson.stockacompose.ui.theme.Gray
+import com.vicksoson.stockacompose.ui.theme.Primary
 
 @Composable
 fun SignUp() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(horizontal = 23.dp, vertical = 44.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        var userName by remember {
+            mutableStateOf(TextFieldValue(""))
+        }
+        var phoneNumber by remember {
+            mutableStateOf(TextFieldValue(""))
+        }
+        var email by remember {
+            mutableStateOf(TextFieldValue(""))
+        }
+        var industry by remember {
+            mutableStateOf(TextFieldValue(""))
+        }
+        // Logo
+        Card(
+            Modifier
+                .padding(top = 10.dp, end = 18.dp)
+                .align(Alignment.End)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.stocka_logo),
+                contentDescription = "stocka logo",
+                modifier = Modifier
+                    .size(48.dp)
+
+            )
+        }
+
+        // Login Form
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+            //greeting
+            Text(text = "Let’s get you started", fontSize = 24.sp, fontWeight = FontWeight.Medium)
+            Text(text = "Create An Account", fontSize = 12.sp, color = Gray)
+
+            //username
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 38.dp)
+            ) {
+                TextField(
+                    value = userName,
+                    onValueChange = {
+                        userName = it
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        cursorColor = Primary,
+                    ),
+                    placeholder = {
+                        Text(text = "Username", color = Color.Gray)
+                    },
+                    modifier = Modifier
+                        .border(1.dp, Gray, RoundedCornerShape(10.dp))
+                        .fillMaxWidth(),
+
+                    )
+            }
+            //password
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 38.dp)
+            ) {
+                TextField(
+                    value = phoneNumber,
+                    onValueChange = {
+                        phoneNumber = it
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        cursorColor = Primary,
+                    ),
+                    placeholder = {
+                        Text(text = "Phone Number", color = Color.Gray)
+                    },
+                    modifier = Modifier
+                        .border(1.dp, Gray, RoundedCornerShape(10.dp))
+                        .fillMaxWidth(),
+
+                    )
+            }
+            //email
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 38.dp)
+            ) {
+                TextField(
+                    value = email,
+                    onValueChange = {
+                        email = it
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        cursorColor = Primary,
+                    ),
+                    placeholder = {
+                        Text(text = "Email Address", color = Color.Gray)
+                    },
+                    modifier = Modifier
+                        .border(1.dp, Gray, RoundedCornerShape(10.dp))
+                        .fillMaxWidth(),
+
+                    )
+            }
+            //industry
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 38.dp)
+            ) {
+                TextField(
+                    value = industry,
+                    onValueChange = {
+                        industry = it
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        cursorColor = Primary,
+                    ),
+                    placeholder = {
+                        Text(
+                            text = "What industry does your business operate in?",
+                            color = Color.Gray
+                        )
+                    },
+                    modifier = Modifier
+                        .border(1.dp, Gray, RoundedCornerShape(10.dp))
+                        .fillMaxWidth(),
+
+                    )
+            }
+
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Primary),
+                modifier = Modifier
+                    .height(50.dp)
+                    .fillMaxWidth(), shape = RoundedCornerShape(10.dp)
+            ) {
+                Text(text = "Continue", color = Color.Black, fontSize = 14.sp)
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Already have an account?",
+                    fontSize = 10.sp,
+                    modifier = Modifier.padding(end = 4.dp)
+                )
+                Text(text = "Login", fontSize = 10.sp, color = Primary)
+            }
+
+        }
+
+
+    }
 }
