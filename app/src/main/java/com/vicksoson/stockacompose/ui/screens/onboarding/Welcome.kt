f2b4e22bd766bead4c513e1840cead7b6da36840
Vcky4 <okon.victor.u@gmail.com>
2023-01-09 22:29:42 +0100

    create welcome screen

---
+++ app/src/main/java/com/vicksoson/stockacompose/ui/screens/onboarding/Welcome.kt
@@ -0,0 +1,16 @@
+package com.vicksoson.stockacompose.ui.screens.onboarding
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Welcome(onEnter: () -> Unit = {}) {
+    Column(Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.weight(0.5f)) {
+
+        }
+    }
+}
