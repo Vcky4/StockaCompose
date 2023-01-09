6e69850567cd50f9cb63a3d8cd6af670c00eb09b
Vcky4 <okon.victor.u@gmail.com>
2023-01-09 23:25:37 +0100

    fix

--- app/src/main/java/com/vicksoson/stockacompose/ui/screens/navigation/MainNav.kt
+++ app/src/main/java/com/vicksoson/stockacompose/ui/screens/navigation/MainNav.kt
@@ -6,12 +6,13 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.vicksoson.stockacompose.ui.screens.home.Home
 import com.vicksoson.stockacompose.ui.screens.navigation.graph.authGraph
+import com.vicksoson.stockacompose.ui.screens.navigation.routes.AuthRoutes
 
 @Composable
 fun MainNav() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "auth") {
+    NavHost(navController = navController, startDestination = AuthRoutes.Default) {
         authGraph(navController = navController)
         composable("home") {
             Home()
